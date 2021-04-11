23f3fbf4d721b96b3e1ac7710ed4fa67c4dc1fa8
ismaelCespedes <ismael.umss@gmail.com>
2021-04-11 09:40:38 -0600

    Faq and footer exercise.

---
+++ app/code/Unit3/LayoutTemplate/Controller/Index/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Unit3\LayoutTemplate\Controller\Index;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+
+    public function execute()
+    {
+       $pageResult = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+       return $pageResult;
+    }
+}
